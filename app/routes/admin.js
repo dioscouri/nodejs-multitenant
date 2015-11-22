c04cd4f21e0010208eeff3eda95db057f82fd7ba
Eugene A. Kalosha <ekalosha@gmail.com>
2015-11-23 00:00:33 +0200

    Fixed client management routes.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,8 +6,8 @@ module.exports = function () {
     var routes = {
         // Client management routes
         'get|/admin/clients': 'admin/clients.js',
-        'get,post|/admin/client/:action': 'admin/clients.js',
-        'get,post|/admin/client/:id/:action': 'admin/clients.js'
+        'get,post|/admin/clients/:action': 'admin/clients.js',
+        'get,post|/admin/clients/:id/:action': 'admin/clients.js'
     };
 
     return routes;
